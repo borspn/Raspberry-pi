95ccca7646e5ff3c2b963d863c981cd4b401ffdf
bohdanborshchevskyi <bohdan.borshchevskyi@fbin.com>
2025-02-19 18:32:11 -0500

    Fix chip select activation logic in Write_Opcode function

--- AS6031_driver/SPI_interface.c
+++ AS6031_driver/SPI_interface.c
@@ -71,12 +71,12 @@ int read_gpio(int pin)
  */
 void Write_Opcode(uint8_t one_byte)
 {
-    gpioWrite(CS_GPIO, 0); // Activate CS (Low)
+    gpioWrite(CS_GPIO, 1); // Activate CS (Low)
     printf("CS_GPIO = %d !\n", gpioRead(CS_GPIO));
     fflush(stdout);
 
     write(&one_byte, 1); // Send opcode
-    gpioWrite(CS_GPIO, 1); // Activate CS (Low)
+    gpioWrite(CS_GPIO, 0); // Activate CS (Low)
     printf("CS_GPIO = %d !\n", gpioRead(CS_GPIO));
     fflush(stdout);
 }
