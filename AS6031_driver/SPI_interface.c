acfae11234b7145bb91733918ba33f95b2abbe23
bohdanborshchevskyi <bohdan.borshchevskyi@fbin.com>
2025-02-19 18:31:01 -0500

    Reorder chip select handling in write function for correct GPIO activation

--- AS6031_driver/SPI_interface.c
+++ AS6031_driver/SPI_interface.c
@@ -48,9 +48,7 @@ void spi_close()
 
 void write(uint8_t *data, int len)
 {
-    gpioWrite(CS_GPIO, 0);
     spiWrite(spi_handle, data, len);
-    gpioWrite(CS_GPIO, 1);
 }
 /**
  * @brief Set GPIO value (HIGH or LOW).
@@ -73,11 +71,12 @@ int read_gpio(int pin)
  */
 void Write_Opcode(uint8_t one_byte)
 {
+    gpioWrite(CS_GPIO, 0); // Activate CS (Low)
     printf("CS_GPIO = %d !\n", gpioRead(CS_GPIO));
     fflush(stdout);
 
     write(&one_byte, 1); // Send opcode
-
+    gpioWrite(CS_GPIO, 1); // Activate CS (Low)
     printf("CS_GPIO = %d !\n", gpioRead(CS_GPIO));
     fflush(stdout);
 }
