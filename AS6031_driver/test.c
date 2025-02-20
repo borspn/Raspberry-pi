06a5bb5c89a74b57be92ec09ee63c9967d362ae6
bohdanborshchevskyi <bohdan.borshchevskyi@fbin.com>
2025-02-20 14:32:35 -0500

    Add test program for GPIO interrupt handling

---
+++ AS6031_driver/test.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <pigpio.h>
+
+#define PIN 24
+
+void callback(int gpio, int level, uint32_t tick)
+{
+    printf("Interrupt on GPIO %d, Level: %d, Time: %u\n", gpio, level, tick);
+}
+
+int main()
+{
+    if (gpioInitialise() < 0)
+    {
+        printf("Failed to initialize GPIO!\n");
+        return 1;
+    }
+
+    gpioSetMode(PIN, PI_INPUT);
+    gpioSetPullUpDown(PIN, PI_PUD_UP);
+
+    if (gpioSetISRFunc(PIN, EITHER_EDGE, 0, callback) < 0)
+    {
+        printf("Failed to set ISR!\n");
+        return 1;
+    }
+
+    printf("Waiting for interrupts...\n");
+    while (1)
+        sleep(1);
+
+    gpioTerminate();
+    return 0;
+}
