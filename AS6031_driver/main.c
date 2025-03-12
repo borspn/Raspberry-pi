1575cfd910411139f3cf884421ea6a705b557ee9
bohdanborshchevskyi <bohdan.borshchevskyi@fbin.com>
2025-03-12 12:18:28 -0400

    Add SPI handle and GPIO line variables to main.c

--- AS6031_driver/main.c
+++ AS6031_driver/main.c
@@ -16,6 +16,12 @@
 #define INTERRUPT_GPIO_PIN 23
 #define CHIPNAME "gpiochip0"
 
+int spi_handle;
+struct gpiod_chip *chip;
+struct gpiod_line *cs_line;
+struct gpiod_line *irq_line;
+
+
 // volatile bool My_INTN_State = false;
 volatile uint8_t My_INTN_State = 1; /* low active */
 
