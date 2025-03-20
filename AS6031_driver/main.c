2d9315225404b6f2aa1712cc8722777ab752a9d0
bohdanborshchevskyi <bohdan.borshchevskyi@fbin.com>
2025-03-20 12:28:12 -0400

    Add debug output for SRR_ERR_FLAG content in My_Time_Conversion_Mode

--- AS6031_driver/main.c
+++ AS6031_driver/main.c
@@ -295,6 +295,8 @@ void My_Time_Conversion_Mode(void)
     /* STEP - Read SRR_ERR_FLAG to check if any error
      *  occurred during last measurement cycle */
     SRR_ERR_FLAG_content = Read_Dword(RC_RAA_RD_RAM, SRR_ERR_FLAG);
+    printf("SRR_ERR_FLAG_content%d\n", SRR_ERR_FLAG_content);
+    fflush(stdout);
 
     if (SRR_ERR_FLAG_content > 0)
     {
