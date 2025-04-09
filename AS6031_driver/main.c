dd09aac6033959e122a66adab026efb008f01459
bohdanborshchevskyi <bohdan.borshchevskyi@fbin.com>
2025-04-09 12:56:55 -0400

    Change MyDiffTOFSumAvg type from float to double for improved precision in calculations

--- AS6031_driver/main.c
+++ AS6031_driver/main.c
@@ -46,7 +46,7 @@ uint32_t MyRAWPWDOWN;
 
 float MyTOFSumAvgUP;
 float MyTOFSumAvgDOWN;
-float MyDiffTOFSumAvg;
+double MyDiffTOFSumAvg;
 float MyRealAMUP;
 float MyRealAMDOWN;
 float MyRealPWUP;
@@ -437,7 +437,7 @@ void Process_TOF(void)
         MyDiffTOFSumAvg_ps = MyDiffTOFSumAvg / 1e-12;
 
         velocity = (abs(MyDiffTOFSumAvg) * (SPEED_OF_SOUND_WATER * SPEED_OF_SOUND_WATER)) / (2 * LEN_OF_SENS);
-        printf("abs(MyDiffTOFSumAvg) = %f\n", MyDiffTOFSumAvg/1e-3);
+        printf("abs(MyDiffTOFSumAvg) = %lf\n", MyDiffTOFSumAvg);
         printf("SPEED_OF_SOUND_WATER = %f\n", SPEED_OF_SOUND_WATER);
         printf("LEN_OF_SENS = %f\n", LEN_OF_SENS);
         fflush(stdout);
