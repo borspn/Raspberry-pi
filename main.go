8f6342a4c3be1fa3ee924f0937d67889dec16ff8
bohdanborshchevskyi <bohdan.borshchevskyi@fbin.com>
2025-06-17 11:40:16 -0400

    Refactor main.go to remove sensor package prefix from function calls

--- main.go
+++ main.go
@@ -3,17 +3,15 @@ package main
 import (
 	"fmt"
 	"time"
-
-	"RaspPi/sensor"
 )
 
 func main() {
-    sensor.InitSensor("spidev0.0", 25)
-    sensor.SensorInit()
+	InitSensor("spidev0.0", 25)
+	SensorInit()
 
-    t := time.NewTicker(10 * time.Second)
-    for fr := range t.C {
-        _ = fr // just wait
-        fmt.Printf("flow: %.6f\n", sensor.ReadFlowRate())
-    }
+	t := time.NewTicker(10 * time.Second)
+	for fr := range t.C {
+		_ = fr // just wait
+		fmt.Printf("flow: %.6f\n", sensor.ReadFlowRate())
+	}
 }
