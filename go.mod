2db92d9898079b3a4bdacc8b6aa019637c88b515
bohdanborshchevskyi <bohdan.borshchevskyi@fbin.com>
2025-06-17 11:22:15 -0400

    Add initial implementation of sensor package with SPI communication and flow rate reading

---
+++ go.mod
@@ -0,0 +1,5 @@
+module RaspPI
+
+go 1.23.4
+
+require golang.org/x/sys v0.33.0
--- main.go
+++ main.go
@@ -11,13 +11,15 @@ var (
 	spiDev   string = "spidev0.0"
 ) // change accordingly
 func main() {
-	InitSPI(chipName, csGPIO, spiDev)
-	// Read and print flow rate every 10 seconds
+
+	sensor.InitSensor(dev, cs) // exported from sensor.go
+	sensor.SensorInit()        // one-shot AS6031 setup
+
 	ticker := time.NewTicker(10 * time.Second)
 	defer ticker.Stop()
 	for {
 		// Read flow rate
-		flowRate := ReadFlowRate()
+		flowRate := sensor.ReadFlowRate()
 		fmt.Printf("Flow Rate: %.6f \n", flowRate)
 		// Wait for next tick
 		<-ticker.C
