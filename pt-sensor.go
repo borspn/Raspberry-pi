966ae4671990c1f574612011c31782ad5cba5b4d
bohdanborshchevskyi <bohdan.borshchevskyi@fbin.com>
2025-06-26 14:46:59 -0400

    Refactor readRaw function to replace bytesToUint32BE with manual bitwise operations for raw pressure and temperature extraction

--- pt-sensor.go
+++ pt-sensor.go
@@ -47,11 +47,11 @@ func (s *Sensor) readRaw() (status byte, rawP, rawT uint32, err error) {
 	status = buf[0]
 	fmt.Println("Status:", status)
 	for i := 0; i < 6; i++ {
-		fmt.Printf("Byte %d: %b\n", i+1, buf[i+1])
+		fmt.Printf("Byte %d: %d\n", i+1, buf[i+1])
 	}
-	rawP = bytesToUint32BE(buf[1:4])
+	rawP = uint32(buf[1])<<16 | uint32(buf[2])<<8 | uint32(buf[3])
 	fmt.Println("Raw Pressure:", rawP)
-	rawT = bytesToUint32BE(buf[4:7])
+	rawT = uint32(buf[4])<<16 | uint32(buf[5])<<8 | uint32(buf[6])
 	fmt.Println("Raw Temperature:", rawT)
 	return
 }
