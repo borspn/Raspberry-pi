8c431c40863fae56296e6fbd16f868fbe0811b26
bohdanborshchevskyi <bohdan.borshchevskyi@fbin.com>
2025-06-26 14:31:47 -0400

    Fix byte output format in readRaw function to use decimal representation

--- pt-sensor.go
+++ pt-sensor.go
@@ -41,7 +41,7 @@ func (s *Sensor) readRaw() (status byte, rawP, rawT uint32, err error) {
 	status = buf[0]
 	fmt.Println("Status:", status)
 	for i := 0; i < 6; i++ {
-		fmt.Printf("Byte %d: %02b\n", i+1, buf[i+1])
+		fmt.Printf("Byte %d: %b\n", i+1, buf[i+1])
 	}
 	rawP = uint32(buf[1])<<16 | uint32(buf[2])<<8 | uint32(buf[3])
 	fmt.Println("Raw Pressure:", rawP)
